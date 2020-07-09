af9dabc9ed0d03b87e62a51d0312bb1a48d1f6df
lsy <shaoyulife@gmail.com>
2020-07-09 15:17:45 +0800

    first commit

---
+++ main.cpp
@@ -0,0 +1,171 @@
+#include <SFML/Graphics.hpp>
+#include "time.h"
+
+using namespace sf;
+
+const int M = 20;
+const int N = 10;
+
+int field[M][N] = {0};
+
+struct Point{
+    int x, y;
+}a[4], b[4];
+
+int figure[7][4] = {
+    1,3,5,7, // I
+    2,4,5,7, // Z
+    3,5,4,6, // S
+    3,5,4,7, // T
+    2,3,5,7, // L
+    3,5,7,6, // J
+    2,3,4,5, // O
+};
+
+bool check();
+
+int main() {
+    srand(time(0));
+    RenderWindow window(VideoMode(320, 480), "Tetris");
+
+    Texture color, bg, frame_pic;
+    color.loadFromFile("images/tiles.png");
+    bg.loadFromFile("images/background.png");
+    frame_pic.loadFromFile("images/frame.png");
+
+    Sprite s(color), background(bg), frame(frame_pic);
+
+    int dx = 0, colorNum = 1;
+    bool rotate = false;
+
+    float delay = 0.3, timer = 0;
+
+    Clock clock;
+
+    while(window.isOpen()){
+        float time = clock.getElapsedTime().asSeconds();
+        clock.restart();
+        timer += time;
+
+        Event e;
+        while(window.pollEvent(e)){
+            if(e.type == Event::Closed){
+                window.close();
+            }
+            if(e.type == Event::KeyPressed){
+                if(e.key.code == Keyboard::Up){
+                    rotate = true;
+                }else if(e.key.code == Keyboard::Left){
+                    dx = -1;
+                }else if(e.key.code == Keyboard::Right){
+                    dx = 1;
+                }
+            }
+        }
+        if(Keyboard::isKeyPressed(Keyboard::Down)){
+            delay = 0.05;
+        }
+
+        //MOVE
+        for(int i = 0 ; i < 4; i++){
+            b[i] = a[i];
+            a[i].x += dx;
+        }
+        if(!check()){
+            for(int i = 0; i < 4; i++){
+                a[i] = b[i];
+            }
+        }
+
+
+        //ROTATE
+        if(rotate){
+            Point p = a[1];
+            for(int i = 0; i < 4; i++){
+                int x = a[i].y - p.y;
+                int y = a[i].x - p.x;
+                a[i].x = p.x - x;
+                a[i].y = p.y + y;
+             }
+            if(!check()){
+                for(int i = 0; i < 4; i++){
+                    a[i] = b[i];
+                }
+            }
+        }
+
+        //TICK
+        if(timer > delay){
+            for(int i = 0; i < 4; i++){
+                b[i] = a[i];
+                a[i].y += 1;
+            }
+            if(!check()){
+                for(int i = 0; i < 4; i++){
+                    field[b[i].y][b[i].x] = colorNum;
+                }
+                colorNum= 1 + rand()%7;
+                int n = rand()%7;
+                for(int i = 0; i < 4; i++){
+                    a[i].x = figure[n][i]%2;
+                    a[i].y = figure[n][i]/2;
+                }
+            }
+            timer = 0;
+        }
+
+
+
+        //check lines
+        int k = M - 1;
+        for(int i = M - 1; i > 0; i--){
+            int cnt = 0;
+            for(int j = 0; j < N; j++){
+                if(field[i][j])
+                    cnt++;
+                field[k][j] = field[i][j];
+            }
+            if(cnt <N){
+                k--;
+            }
+        }
+
+        dx=0; rotate=0; delay=0.3;
+
+        window.clear(Color::White);
+        window.draw(background);
+
+        for(int i = 0; i < M; i++){
+            for(int j = 0; j < N; j++){
+                if(field[i][j] == 0){
+                    continue;
+                }
+                s.setTextureRect(IntRect(field[i][j]*18, 0, 18, 18));
+                s.setPosition(j*18, i*18);
+                s.move(28,31);
+                window.draw(s);
+            }
+        }
+        for(int i = 0; i < 4; i++){
+            s.setTextureRect(IntRect(colorNum*18, 0, 18, 18));
+            s.setPosition(a[i].x*18, a[i].y*18);
+            s.move(28,31);
+            window.draw(s);
+        }
+        window.draw(frame);
+        window.display();
+    }
+    return 0;
+}
+
+bool check(){
+    for(int i = 0; i < 4; i++){
+        if(a[i].x < 0 || a[i].x >= N || a[i].y >= M){ //not in the range
+            return false;
+        }else if(field[a[i].y][a[i].x]){
+            return false;
+        }
+    }
+    return true;
+}
+
